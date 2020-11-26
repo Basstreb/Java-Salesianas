f1a8d785003193137e1604914f3b6e3d7434abb0
Daniel <danlozcam@gmail.com>
2020-11-26 11:39:04 +0100

    Conseguido que funcione boolean

--- Ejercicio_Class/src/ejercicio_class/Vehiculo.java
+++ Ejercicio_Class/src/ejercicio_class/Vehiculo.java
@@ -48,13 +48,13 @@ public class Vehiculo {
 		@SuppressWarnings({ "unused", "resource" })
 		Scanner teclado = new Scanner(System.in);
 		
-		Vehiculo coche1 = new Vehiculo("Mustang");
-		coche1.setPotencia(550);
-		coche1.setTraccion(false);
-		
-		Vehiculo coche2 = new Vehiculo("Ibiza");
-		coche2.setPotencia(80);
-		coche2.setTraccion(true);
+//		Vehiculo coche1 = new Vehiculo("Mustang");
+//		coche1.setPotencia(550);
+//		coche1.setTraccion(false);
+//		
+//		Vehiculo coche2 = new Vehiculo("Ibiza");
+//		coche2.setPotencia(80);
+//		coche2.setTraccion(true);
 		
 		System.out.println("Indique el modelo del coche");
 		Vehiculo coche3 = new Vehiculo(teclado.nextLine());
@@ -63,17 +63,23 @@ public class Vehiculo {
 		coche3.setPotencia(teclado.nextDouble());
 		
 		System.out.println("Indique con un Si o un No si tiene traccion a las 4 Ruedas");
-		String rueda = teclado.nextLine();
+		String rueda;
+		rueda = teclado.next();
+		String traccion = null;
 		
-//		if (rueda.equals("Si")) {
-//			coche3.setTraccion(true);
-//		}
-//		else if (rueda.equals("No")) {
-//			coche3.setTraccion(false);
-//		}
+		if (rueda.equals("Si")) {
+			traccion = "true";
+		}
+		else if (rueda.equals("No")) {
+			traccion = "false";
+		}
 		
-		coche1.imprimir();
-		coche2.imprimir();
+		boolean trac = Boolean.parseBoolean(traccion);
+		
+		coche3.setTraccion(trac);
+		
+//		coche1.imprimir();
+//		coche2.imprimir();
 		coche3.imprimir();
 		
 		
