6340cbd53c69e62e22310b603fa5c202171664f0
Daniel <danlozcam@gmail.com>
2021-01-19 21:49:59 +0100

    Acabamos Retos

--- Lozano_Campano_Daniel_PROG_Retos/src/laslosetas/LasLosetas.java
+++ Lozano_Campano_Daniel_PROG_Retos/src/laslosetas/LasLosetas.java
@@ -6,16 +6,19 @@ public class LasLosetas {
 
 	public static void main(String[] args) {
 
+		System.out.println("Introduzca numero de habitaciones a comprobar");
 		int n = Metodos.tryCatchInt();
 		int alto;
 		int ancho;
 		
 		for (int i = 0; i < n; i++) {
 
+			System.out.println("Introduzca el ancho de la habitacion");
 			do {
 				ancho = Metodos.tryCatchInt();
 			} while (Metodos.num < 0 || Metodos.num > 1000);
-				
+			
+			System.out.println("Introduzca el alto de la habitacion");
 			do {
 				alto = Metodos.tryCatchInt();
 			} while (Metodos.num < 0 || Metodos.num > 1000);
