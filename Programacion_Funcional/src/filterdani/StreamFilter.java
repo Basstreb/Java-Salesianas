3fcc44b030c7cd27ce36918f24086a2974e9ee59
dani <dani@192.168.31.206>
2021-02-15 11:54:46 +0100

    Metemos muchos ejemplo de la api stream

---
+++ Programacion_Funcional/src/filterdani/StreamFilter.java
@@ -0,0 +1,13 @@
+package filterdani;
+
+import java.time.LocalDate;
+
+public class StreamFilter {
+
+	public static void main(String[] args) {
+		LocalDate edad = LocalDate.of(1992, 11, 5);
+		
+		System.out.printf("%s", edad);
+	}
+	
+}
